4008c9b6dc1fac8423dfadccfe2903cb9b09a7bc
Daniel Larsen <dalars@microsoft.com>
2019-07-08 12:27:47 +1200

    Display test from a Blob

---
+++ StorageService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloAspDotNetCore
+{
+    public class StorageService
+    {
+        private static readonly Lazy<CloudBlobClient> _lazyClient = new Lazy<CloudBlobClient>(InitializeCloudBlobClient);
+
+        private static CloudBlobClient CloudBlobClient => _lazyClient.Value;
+
+        public static bool StorageIsConfigured()
+        {
+            return !string.IsNullOrEmpty(Startup.Configuration["Blob.Path"])
+                && !string.IsNullOrEmpty(Startup.Configuration["Blob.StorageConnectionString"]);
+        }
+
+        public async static Task<string> GetBlobContent()
+        {
+            try
+            {
+                // get from Blob
+                ICloudBlob blob = await CloudBlobClient.GetBlobReferenceFromServerAsync(
+                    new Uri($"{CloudBlobClient.BaseUri}{Startup.Configuration["Blob.Path"]}"));
+
+                using (var stream = new MemoryStream())
+                {
+                    await blob.DownloadToStreamAsync(stream);
+                    stream.Seek(0, SeekOrigin.Begin);
+                    return Encoding.UTF8.GetString(stream.ToArray());
+                }
+            }
+            catch (Exception ex)
+            {
+                // NOTE: This is Test code - do not do this in Production
+                return ex.Message;
+            }
+        }
+
+        private static CloudBlobClient InitializeCloudBlobClient()
+        {
+            if (Startup.Configuration["Blob.StorageConnectionString"] == null)
+                throw new InvalidOperationException("App Setting \"Blob.StorageConnectionString\" is not set.");
+
+            var account = CloudStorageAccount.Parse(Startup.Configuration["Blob.StorageConnectionString"]);
+            return account.CreateCloudBlobClient();
+        }
+    }
+}
